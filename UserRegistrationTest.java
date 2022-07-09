f4a63db5ced43e61ff4220606726a833f10f8eb4
JVignesh4 <vigneshbuji04@gmail.com>
2022-07-09 20:45:18 +0530

    Added: Validating Password For Minimum One Upper Case

--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -31,7 +31,7 @@ public class UserRegistrationTest {
     }
 
     public void validatePassword(String password){
-        storePattern.setPasswordPattern("^[a-zA-Z0-9]{8,}$");
+        storePattern.setPasswordPattern("^[A-Z]{1}?:[a-zA-Z][_]@.[0-9]{7,}$");
         String passPattern= storePattern.getPasswordPattern();
         storePattern.validatingInput(password,passPattern,"Password");
     }
