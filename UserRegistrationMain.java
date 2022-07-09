4415a9d86bbe592aff9da069b4a6a2e7d96ddda4
JVignesh4 <vigneshbuji04@gmail.com>
2022-07-09 14:25:10 +0530

    Added: Validating User Last Name

--- UserRegistrationMain.java
+++ UserRegistrationMain.java
@@ -3,18 +3,25 @@ package com.blz.regex;
 import java.util.Scanner;
 
 public class UserRegistrationMain {
+
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration using regex program");
-        UserRegistrationTest validate = new UserRegistrationTest();
+        UserRegistrationTest validate= new UserRegistrationTest();
         Scanner scan = new Scanner(System.in);
         System.out.println("1.To Validate the First name.");
+        System.out.println("2.To Validate the Last Name.");
         System.out.println("Enter the option number to perform the operation");
         int option = scan.nextInt();
-        switch (option) {
+        switch(option){
             case 1:
-                System.out.println("Enter the First Name. Note: first letter should be capital");
+                System.out.println("Enter the First Name(Note: First letter should be capital): ");
                 String firstName = scan.next();
                 validate.validateFirstName(firstName);
+                break;
+            case 2:
+                System.out.println("2.Enter the Last name(Note:First letter should be capital): ");
+                String lastName = scan.next();
+                validate.validateLastName(lastName);
                 break;
             default:
                 System.out.println("Please Enter valid option to validate the pattern");
--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -2,10 +2,16 @@ package com.blz.regex;
 
 public class UserRegistrationTest {
 
-        public void validateFirstName(String firstName){
-            UserInformation store =new UserInformation();
-            store.setFirstName("^[A-Z]{1}[a-z]{3,}$");
-            String first= store.getFirstName();
-            store.validatingInput(firstName,first,"FirstName");
-        }
+    public void validateFirstName(String firstName){
+        UserInformation store =new UserInformation();
+        store.setFirstName("^[A-Z]{1}[a-z]{3,}$");
+        String first= store.getFirstName();
+        store.validatingInput(firstName,first,"FirstName");
+    }
+    public void validateLastName(String lastName){
+        UserInformation f=new UserInformation();
+        f.setLastName("^[A-Z]{1}[a-z]{3,}$");
+        String last= f.getLastName();
+        f.validatingInput(lastName,last,"LastName");
+    }
 }
